bf8af4f9a2f543302ae7c69edb694758cfb6a7cb
Hitesh Riziya <hriziya@gmail.com>
2020-07-09 19:45:06 +0530

    Latest changes

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
 import * as React from "react";
 import { Button } from "@chakra-ui/core";
-import { Image } from "@chakra-ui/core";
-import {
-  Input,
-  InputGroup,
-  InputLeftElement,
-  InputRightElement
-} from "@chakra-ui/core";
 import { Stack } from "@chakra-ui/core";
 import {
   chakra,
@@ -15,6 +8,7 @@ import {
   useColorModeValue
 } from "@chakra-ui/system";
 import Head from "next/head";
+import { PrismaClient } from "@prisma/client";
 
 function Switcher() {
   const { toggleColorMode: toggleMode } = useColorMode();
@@ -22,37 +16,29 @@ function Switcher() {
   return <button onClick={toggleMode}>Current mode: {text}</button>;
 }
 
-const InputGrouper = () => {
-  const [bool, setBool] = React.useState(false);
+async function TotalSummary() {
+  const prisma = new PrismaClient();
+  const allUsers = await prisma.hubstaff_users.findMany();
+  console.log(allUsers);
+
+  // const [bool, setBool] = React.useState(false);
   return (
     <>
-      <InputGroup maxWidth="400px">
-        <InputLeftElement color="gray.300" fontSize="1.2em" children="$23" />
-        <Input placeholder="Enter amount" />
-        {bool && <InputRightElement children={"C"} />}
-      </InputGroup>
-      <button onClick={() => setBool(s => !s)}>Add 1Right Element</button>
+      <div> {allUsers.length} Users. </div>
       <br />
     </>
   );
-};
+}
 
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>Weenggs CMS</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
       <chakra.div fontSize="20px">Welcome to chakra</chakra.div>
-      <Image
-        src="https://bit.ly/sage-adebayo"
-        fallbackSrc="https://via.placeholder.com/240"
-        fit="cover"
-        width="400px"
-        height="300px"
-      />
 
       <chakra.div bg="gray.800" padding={4}>
         <DarkMode>
@@ -60,19 +46,12 @@ const Home = () => (
         </DarkMode>
       </chakra.div>
 
-      <InputGroup>
-        <InputLeftElement children={"+234"} />
-        <Input type="phone" placeholder="Phone number" />
-      </InputGroup>
-
-      <InputGrouper />
+      {/* <TotalSummary /> */}
 
       <Switcher />
 
       <Stack direction="row" spacing="40px">
-        <div>Welcome home</div>
-        <div>Welcome home</div>
-        <div>Welcome home3</div>
+        <div>{"Something will be printed here.."}</div>
       </Stack>
     </main>
   </div>
